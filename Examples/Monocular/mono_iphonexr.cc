0e64d8fa874f897fe67bc5c25a3b9676a4cdc564
Kim <kisv701@gmail.com>
2022-09-06 23:34:34 +0200

    Add example for running on videos recorded with iphone xr

---
+++ Examples/Monocular/mono_iphonexr.cc
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<opencv2/core/core.hpp>
+
+#include<System.h>
+
+int main(int argc, char **argv)
+{  
+    // Video file to do slam on.
+    if (argc != 2) {
+        std::cout << "Usage from main folder:\n$ ./Examples/Monocular/mono_iphonexr <path to your video file>\nUnexpected number of arguments found. Exiting...\n";
+        return 1;
+    }
+    std::string file_path(argv[1]);
+    std::cout << "Working on " << file_path << "\n";
+    cv::VideoCapture cap(file_path);
+
+    cout << endl << "-------" << endl;
+    cout.precision(17);
+
+
+    int fps = 20;
+    float dT = 1.f/fps;
+
+    std::string orb_vocabulary_path = "./Vocabulary/ORBvoc.txt";
+    std::string settings_path = "./Examples/Monocular/IphoneXR.yaml";
+
+    bool enablePanglinVisualization = false;
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(orb_vocabulary_path, settings_path, ORB_SLAM3::System::MONOCULAR, enablePanglinVisualization);
+    float imageScale = SLAM.GetImageScale();
+
+    std::cout << "Image scale: " << imageScale << std::endl;
+
+    double t_resize = 0.f;
+    double t_track = 0.f;
+
+    // Main loop
+    cv::Mat image;
+    int proccIm = 0;
+    int frame_count = 0;
+    while(1)
+    {
+
+        // Read image from file
+        cap >> image; 
+        double tframe = frame_count * dT;
+
+        if(image.empty())
+        {
+            cerr << endl << "Failed to load image at, end of video?\n";
+            break; // Break out of loop to go to shutdown procedures.
+        }
+
+        if(imageScale != 1.f)
+        {
+
+            int width = image.cols * imageScale;
+            int height = image.rows * imageScale;
+            cv::resize(image, image, cv::Size(width, height));
+        }
+
+        // Pass the image to the SLAM system
+        // cout << "tframe = " << tframe << endl;
+        SLAM.TrackMonocular(image, tframe);
+
+        // Wait to load the next frame
+        
+    }
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Save camera trajectory
+    if (1)
+    {
+        const string kf_file =  "kf_" + file_path + ".txt";
+        const string f_file =  "f_" + file_path + ".txt";
+        SLAM.SaveTrajectoryEuRoC(f_file);
+        SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
+    }
+    else
+    {
+        SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
+        SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
+    }
+
+    return 0;
+}
